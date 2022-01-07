da1726ed507c021761ae43ab059b015453e8c9dc
ishwar singh <icr7.dev@outlook.com>
2022-01-07 08:44:20 +0530

    modification

--- LinkList/FillingLinkedList.java
+++ LinkList/FillingLinkedList.java
@@ -1,34 +1,35 @@
 package com.icr7.leetcode.linkedlist;
 
+import java.util.Scanner;
+
 public class FillingLinkedList {
 
 	public static void main(String[] args) {
-		LinkedList myList[] = new LinkedList[5];
+		Scanner sc = new Scanner(System.in);
+		LinkedList myList = new LinkedList();
+		LinkedList head = myList;
+		LinkedList tail = myList;
+
 		
-		for (int i = 0; i < 5; i++) {
-			myList[i] = new LinkedList();
+		for(int i=1;i<101;i++) {
+			tail.push(i*2);
+			tail=tail.getNext();
 			
-			myList[i].setData((i + 1) * 5);
-			if (i == 4) {
-				myList[i].setNext(null);
-			} else {
-				myList[i + 1] = new LinkedList();
-				myList[i].setNext(myList[i + 1]);
-			}
 		}
 
-		for (int i = 0; i < 5; i++) {
-			System.out.println(myList[i].getData());
+		for(int i=1;i<101;i++) {
+			System.out.println(head.getData());
+			head=head.getNext();
 		}
-
+		
 	}
 
 }
 
 class LinkedList {
-	int data;
-	LinkedList nextLinkedList;
-
+	private int data;
+	private LinkedList nextLinkedList;
+	int headcount=0;
 	public void setData(int data) {
 		this.data = data;
 	}
@@ -42,6 +43,22 @@ class LinkedList {
 	}
 
 	public LinkedList getNext() {
-		return this.nextLinkedList;
+		return nextLinkedList;
 	}
+	
+	public void push(int data) {
+		LinkedList newList=new LinkedList();
+		if(headcount==0) {
+			this.data=data;
+			this.nextLinkedList=newList;
+			
+		}
+		else {
+		
+		newList.setData(data);
+		this.nextLinkedList=newList;
+		headcount++;
+		}
+	}
+		
 }
