8eb5455094b64c7e6ac940a6cf6cba3e750383fd
ishwar singh <icr7.dev@outlook.com>
2022-01-07 09:28:28 +0530

    CreateAndDisplayLinkedList javatpoint

---
+++ LinkList/CreateAndDisplayLinkedList.java
@@ -0,0 +1,55 @@
+package com.icr7.leetcode.linkedlist;
+
+public class CreateAndDisplayLinkedList {
+
+	public static void main(String[] args) {
+		LinkedList myList=new LinkedList();
+		myList.push(1);
+		myList.push(2);
+		myList.push(3);
+		myList.push(4);
+		myList.push(5);
+		myList.push(6);
+		myList.push(7);
+		myList.display();
+
+	}
+
+}
+class LinkedList{
+	
+	class Node{
+		public int data;
+		public Node next;
+		
+		Node(int data){
+			this.data = data;
+			this.next=null;
+		}
+	}
+	
+	public Node head=null;
+	public Node tail=null;
+	
+	public void push(int data) {
+		Node newNode= new Node(data);
+	
+		if(head==null) {
+			head=newNode;
+			tail=newNode;
+		}
+		else {
+			tail.next=newNode;
+			tail=newNode;
+		}		
+	}
+	
+	public void display() {
+		Node curr= head;
+		while(curr!=null) {
+			System.out.println(curr.data);
+			curr=curr.next;
+		}
+	}
+	
+}
