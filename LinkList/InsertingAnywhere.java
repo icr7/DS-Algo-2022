285bcdeff592cf1038d72874d2c5553a395466f3
ishwar singh <icr7.dev@outlook.com>
2022-01-12 09:09:38 +0530

    adding feature to push at top and anywhere and get size also

--- LinkList/InsertingAnywhere.java
+++ LinkList/InsertingAnywhere.java
@@ -11,18 +11,28 @@ public class InsertingAnywhere {
 		System.out.println("push data");
 		while (true) {
 			n = sc.nextInt();
-			if (n == 0) {
+			if (n == 0) { // push element until enter 0
 				break;
 			} else {
 				list.push(n);
 			}
 		}
 
-		System.out.println("before inserting element");
+		System.out.println("Before inserting element");
 		list.display();
-		list.push(9, sc.nextInt());
-		System.out.println("\nafter inserting element");
+		System.out.println("Size: "+list.size());
+
+		System.out.println("Enter element you want to push: ");
+		int element = sc.nextInt();
+
+		System.out.println("Enter postion where you want to push: ");
+		int pos = sc.nextInt();
+
+		list.push(element, pos);
+
+		System.out.println("After inserting element");
 		list.display();
+		System.out.println("Size: "+list.size());
 
 	}// main
 
@@ -41,26 +51,39 @@ class LinkedList {
 
 	Node head = null;
 	Node tail = null;
-
+    int size=0;
 	public void push(int data) {
 		Node newNode = new Node(data);
 		if (head == null) {
 			head = newNode;
 			tail = newNode;
+			size++;
 		} else {
 			tail.nextNode = newNode;
 			tail = tail.nextNode;
+			size++;
 		}
 	}
 
 	public void push(int data, int pos) {
 		Node newNode = new Node(data);
 		Node currentNode = head;
-		for (int i = 0; i < pos - 1; i++) {
-			currentNode = currentNode.nextNode;
+		if (pos == 1) {
+			pushAtTop(newNode);
+		} else {
+			for (int i = 1; i < pos - 1; i++) {
+				currentNode = currentNode.nextNode;
+			}
+			newNode.nextNode = currentNode.nextNode;
+			currentNode.nextNode = newNode;
+			size++;
 		}
-		newNode.nextNode = currentNode.nextNode;
-		currentNode.nextNode = newNode;
+	}
+
+	public void pushAtTop(Node node) {
+		node.nextNode = head;
+		head = node;
+		size++;
 	}
 
 	public void display() {
@@ -69,5 +92,9 @@ class LinkedList {
 			System.out.print(currentNode.data + " ");
 			currentNode = currentNode.nextNode;
 		}
+	}
+	
+	public int size() {
+		return this.size;
 	}
 }
