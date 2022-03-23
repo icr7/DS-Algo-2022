db7225a4fde651c6b8dbff45010a28b7c378318f
ishwar singh <icr7.dev@outlook.com>
2022-03-23 08:13:57 +0530

    Hashmap with unique Key only

---
+++ HashMap/HashMap2.java
@@ -0,0 +1,93 @@
+
+public class HashMap2 {
+    public static void main(String[] args) {
+        Map myMap = new Map();
+        myMap.add(0, "Ronaldo");
+        myMap.add(1, "Ishwar");
+        myMap.add(2, "Mohit");
+        myMap.add(3, "Deepak");
+        myMap.add(4, "Lalit");
+        myMap.add(5, "Suresh");
+        myMap.add(7, "Jack");
+        myMap.add(7, "David");
+        myMap.add(7, "Rock");
+        myMap.add(8, "Tarzen");
+        myMap.add(8, "Tommy");
+        myMap.add(9, "Dhoni");
+        myMap.add(10, "Sachin");
+
+        for (int i = 0; i < 10; i++) {
+            if (myMap.myNode[i] != null)
+                System.out.println(i+". "+myMap.myNode[i].toString());
+            else
+                System.out.println(i+". "+"null");
+
+        }
+    }
+}
+
+class Map {
+    Node[] myNode = new Node[10];
+
+    public void add(Integer key, String value) {
+        int hash = key.hashCode();
+        if (hash < 0) {
+            hash = hash * -1;
+        }
+        int hashIndex = hash % (10 - 1);
+        Node curr = myNode[hashIndex];
+          while(true){
+
+            if(curr==null){
+               curr=new Node(key,value,hash);
+               myNode[hashIndex]=curr;
+               break;
+            }
+            else if( curr.key==key){
+                curr=replaceNode(curr,new Node(key,value,hash));
+                myNode[hashIndex]=curr;
+                break;
+            }
+            else{
+                if(curr.next==null) {
+                    curr.next = new Node(key, value, hash);
+                    break;
+                }
+                else
+                curr=curr.next;
+
+            }
+        }
+    }
+
+    Node replaceNode(Node curr, Node newNode ){
+       newNode.next=curr.next;
+       curr=newNode;
+        return curr;
+    }
+
+    class Node {
+        int key;
+        String value;
+        long hash;
+        Node next;
+
+        Node(int key, String value, long hash) {
+            this.key = key;
+            this.value = value;
+            this.hash = hash;
+            this.next = null;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "key='" + key + '\'' +
+                    ", value='" + value + '\'' +
+                    ", hash=" + hash +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+}
+
